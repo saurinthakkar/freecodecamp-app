3b6cec2c3758f0bbb4e29191095861e64d3e3d1c
saurin thakkar <saurin.thakkar13@gmail.com>
2021-10-30 17:41:06 +0530

    Added css file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h2>This is saurin. This is my first component</h2>;
+//Css
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/51Zky37XqoL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt="Young Pandavas 2"
+  ></img>
+);
+
+const Title = () => <h1>Young Pandavas Book 2: The School For Warriors</h1>;
+const Author = () => <h4>Anupam Arunachalam</h4>;
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hello World")
+//   );
+// };
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// };
+
+// function Greeting() {
+//   return <h2>hello World</h2>;
+// }
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
