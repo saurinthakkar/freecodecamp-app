55558ea4b6c17baf309d9e03481b02f692ca6180
saurin thakkar <saurin.thakkar13@gmail.com>
2021-11-14 00:11:33 +0530

    added event basics functionality

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/515Y-kC7EQL._SX310_BO1,204,203,200_.jpg",
     title: "The Blue Umbrella",
     author: "Ruskin Bond",
@@ -14,25 +15,52 @@ const books = [
   },
 
   {
+    id: 2,
     img: "https://images-eu.ssl-images-amazon.com/images/I/51Zky37XqoL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "Young Pandavas Book 2: The School For Warriors",
     author: "Anupam Arunachalam",
     alt: "Young Pandavas 2",
   },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51BxuGY8DDL._SX386_BO1,204,203,200_.jpg",
+    title: "Space - My Knowledge Book",
+    author: "Pegasus Team and Yes",
+    alt: "Space - My Knowledge Book",
+  },
 ];
 
+//const names = ["saurin", "raj", "james"];
+// const newNames = names.map((newName) => {
+//   return <h1>{newName}</h1>;
+// });
+//console.log(newNames);
 function BookList() {
-  return <section className="booklist">{books}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book, index) => {
+        const { img, title, author } = book;
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  // console.log(props);
+  // const { img, title, author } = props;
+  const clickHandler = () => {
+    alert("Hello World");
+  };
+
   return (
     <article className="book">
       <img src={img} alt=""></img>
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Test Example
+      </button>
     </article>
   );
 };
